79c8d5ca6f2d9fc437814072d9f33aaa9fc4027f
usonia09 <uwasonia2001@gmail.com>
2024-04-06 21:13:23 -0400

    map insertion

--- src/routes/+page.svelte
+++ src/routes/+page.svelte
@@ -1,2 +1,35 @@
-<h1>Welcome to SvelteKit</h1>
-<p>Visit <a href="https://kit.svelte.dev">kit.svelte.dev</a> to read the documentation</p>
+<script>
+    import mapboxgl from "mapbox-gl";
+    import "../../node_modules/mapbox-gl/dist/mapbox-gl.css";
+    import { onMount } from "svelte";
+
+
+    mapboxgl.accessToken = import.meta.env.VITE_MY_TOKEN;
+    const myVal = import.meta.env.VITE_MY_TOKEN;
+
+    onMount(() => {
+	let map = new mapboxgl.Map({
+		/* options */
+        container: "map",
+        style: "mapbox://styles/mapbox/streets-v12",
+        center: [-71.3679725, 42.40010974514135],
+        zoom: 10,
+	});
+   })
+
+
+</script>
+
+<h1>Boston Bike System</h1>
+<h1>{myVal}</h1>
+<div id="map"/>
+
+<style>
+    @import url("$lib/global.css");
+    #map {
+	flex: 1;
+    background-color: blue;
+    }
+
+
+</style>
